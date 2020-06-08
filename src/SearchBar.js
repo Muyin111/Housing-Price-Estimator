3a83f77c85e1f7991756d470a63d5e4249f8238a
Muyin111 <47198303+Muyin111@users.noreply.github.com>
2020-06-08 01:25:37 -0400

    Update SearchBar.js

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
 
 export class SearchBar extends Component {
+
+    constructor() {
+        super()
+        this.zipcode = ''
+    }
+
+    handleChange = (event) => {
+        this.zipcode = event.target.value
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.props.handleButtonClick(this.zipcode, e)
+    }
     render() {
         return (
 
             <div style={searchStyle}>
-                <form style={{ display: 'flex' }}>
+                <form style={{ display: 'flex' }} onSubmit={this.handleSubmit}>
                     <input
                         type="text"
                         name="title"
+                        onChange = {this.handleChange}
                         style={{ flex: '10', padding: '10px' }}
                         placeholder="Enter Zipcode ..."
                     />
                     <input
                         type="submit"
                         value="Submit"
-                        className="btn"
-                        style={{ flex: '1' }}
+                        style={{marginLeft: '5px', color: 'white', backgroundColor: 'black', flex: '1' }}
                     />
                 </form>
             </div>
@@ -27,10 +41,13 @@ export class SearchBar extends Component {
 
 const searchStyle = {
    
-    width: '70%',
+    width: '50%',
     float: 'center',
     padding: '20px',
-    margin: '150px',
+    fontSize: '20px',
+    marginTop: '0px',
+    marginLeft: '25%',
+    marginRight: '25%',
     border: '5px'
 }
 
